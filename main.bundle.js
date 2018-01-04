64794ee8a0483c2223d62d9fc43083fa0d36236e
Justin <justin@homeceuconnection.com>
2018-01-04 07:05:26 -0600

    updated page

--- main.bundle.js
+++ main.bundle.js
@@ -86,6 +86,12 @@ webpackEmptyAsyncContext.id = "../../../../../$$_lazy_route_resource lazy recurs
 /* harmony import */ var __WEBPACK_IMPORTED_MODULE_0__angular_core__ = __webpack_require__("../../../core/esm5/core.js");
 /* harmony import */ var __WEBPACK_IMPORTED_MODULE_1__angular_platform_browser__ = __webpack_require__("../../../platform-browser/esm5/platform-browser.js");
 /* harmony import */ var __WEBPACK_IMPORTED_MODULE_2__angular_common__ = __webpack_require__("../../../common/esm5/common.js");
+/* harmony import */ var __WEBPACK_IMPORTED_MODULE_3__angular_router__ = __webpack_require__("../../../router/esm5/router.js");
+/* harmony import */ var __WEBPACK_IMPORTED_MODULE_4__ng_ui_libraries_forms__ = __webpack_require__("../../../../@ng-ui-libraries/forms/@ng-ui-libraries/forms.es5.js");
+/* harmony import */ var __WEBPACK_IMPORTED_MODULE_5__ng_ui_libraries_prompt__ = __webpack_require__("../../../../@ng-ui-libraries/prompt/@ng-ui-libraries/prompt.es5.js");
+/* harmony import */ var __WEBPACK_IMPORTED_MODULE_6__Component_PromptDemoComponent__ = __webpack_require__("../../../../../src/app/Component/PromptDemoComponent.ts");
+/* harmony import */ var __WEBPACK_IMPORTED_MODULE_7__Pages_IndexPageComponent__ = __webpack_require__("../../../../../src/app/Pages/IndexPageComponent.ts");
+/* harmony import */ var __WEBPACK_IMPORTED_MODULE_8__routes__ = __webpack_require__("../../../../../src/app/routes.ts");
 var __decorate = (this && this.__decorate) || function (decorators, target, key, desc) {
     var c = arguments.length, r = c < 3 ? target : desc === null ? desc = Object.getOwnPropertyDescriptor(target, key) : desc, d;
     if (typeof Reflect === "object" && typeof Reflect.decorate === "function") r = Reflect.decorate(decorators, target, key, desc);
@@ -98,15 +104,25 @@ var __metadata = (this && this.__metadata) || function (k, v) {
 
 
 
+
+
+
+
+
+
 var AppComponent = /** @class */ (function () {
-    function AppComponent() {
+    function AppComponent(router) {
+        this.router = router;
     }
     AppComponent = __decorate([
-        Object(__WEBPACK_IMPORTED_MODULE_0__angular_core__["m" /* Component */])({
+        Object(__WEBPACK_IMPORTED_MODULE_0__angular_core__["n" /* Component */])({
             selector: 'app',
-            template: "\n        <div>It works!</div>\n    "
+            template: "\n        <nav class=\"navbar navbar-expand-lg navbar-dark bg-dark\">\n            <div class=\"container\">\n                <a class=\"navbar-brand\" routerLink=\"\">\n                    <img class=\"angular-logo\" src=\"assets/angular.png\"/>\n                    ng-ui-libraries\n                </a>\n                <button class=\"navbar-toggler\" type=\"button\" data-toggle=\"collapse\" data-target=\"#navbarContent\">\n                    <span class=\"navbar-toggler-icon\"></span>\n                </button>\n                <div class=\"collapse navbar-collapse\" id=\"navbarContent\">\n                    <ul class=\"navbar-nav mr-auto\">\n                        <li class=\"nav-item\">\n                            <a routerLink=\"prompt\" class=\"nav-link\" [routerLinkActive]=\"['active']\">\n                                Prompt\n                            </a>\n                        </li>\n                        <li class=\"nav-item\">\n                            <a routerLink=\"forms\" class=\"nav-link\" [routerLinkActive]=\"['active']\">\n                                Forms\n                            </a>\n                        </li>\n                    </ul>\n                </div>\n            </div>\n        </nav>\n        <router-outlet></router-outlet>\n    ",
+            styles: [
+                "\n            .angular-logo {\n                max-height: 45px;\n            }\n        "
+            ]
         }),
-        __metadata("design:paramtypes", [])
+        __metadata("design:paramtypes", [__WEBPACK_IMPORTED_MODULE_3__angular_router__["a" /* Router */]])
     ], AppComponent);
     return AppComponent;
 }());
@@ -115,13 +131,22 @@ var AppModule = /** @class */ (function () {
     function AppModule() {
     }
     AppModule = __decorate([
-        Object(__WEBPACK_IMPORTED_MODULE_0__angular_core__["E" /* NgModule */])({
-            declarations: [AppComponent],
+        Object(__WEBPACK_IMPORTED_MODULE_0__angular_core__["J" /* NgModule */])({
+            declarations: [
+                AppComponent,
+                __WEBPACK_IMPORTED_MODULE_7__Pages_IndexPageComponent__["a" /* IndexPageComponent */],
+                __WEBPACK_IMPORTED_MODULE_6__Component_PromptDemoComponent__["a" /* PromptDemoComponent */]
+            ],
             imports: [
                 __WEBPACK_IMPORTED_MODULE_1__angular_platform_browser__["a" /* BrowserModule */],
-                __WEBPACK_IMPORTED_MODULE_2__angular_common__["a" /* CommonModule */]
+                __WEBPACK_IMPORTED_MODULE_2__angular_common__["b" /* CommonModule */],
+                __WEBPACK_IMPORTED_MODULE_5__ng_ui_libraries_prompt__["a" /* PromptModule */],
+                __WEBPACK_IMPORTED_MODULE_3__angular_router__["b" /* RouterModule */].forRoot(__WEBPACK_IMPORTED_MODULE_8__routes__["a" /* routes */]),
+                __WEBPACK_IMPORTED_MODULE_4__ng_ui_libraries_forms__["b" /* FormsDemoModule */]
             ],
-            exports: [AppComponent],
+            exports: [
+                AppComponent
+            ],
             providers: [],
             bootstrap: [AppComponent]
         })
@@ -129,6 +154,109 @@ var AppModule = /** @class */ (function () {
     return AppModule;
 }());
 
+
+
+/***/ }),
+
+/***/ "../../../../../src/app/Component/PromptDemoComponent.ts":
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+/* harmony export (binding) */ __webpack_require__.d(__webpack_exports__, "a", function() { return PromptDemoComponent; });
+/* harmony import */ var __WEBPACK_IMPORTED_MODULE_0__angular_core__ = __webpack_require__("../../../core/esm5/core.js");
+var __decorate = (this && this.__decorate) || function (decorators, target, key, desc) {
+    var c = arguments.length, r = c < 3 ? target : desc === null ? desc = Object.getOwnPropertyDescriptor(target, key) : desc, d;
+    if (typeof Reflect === "object" && typeof Reflect.decorate === "function") r = Reflect.decorate(decorators, target, key, desc);
+    else for (var i = decorators.length - 1; i >= 0; i--) if (d = decorators[i]) r = (c < 3 ? d(r) : c > 3 ? d(target, key, r) : d(target, key)) || r;
+    return c > 3 && r && Object.defineProperty(target, key, r), r;
+};
+
+var PromptDemoComponent = /** @class */ (function () {
+    function PromptDemoComponent() {
+        this.model = {
+            yesClicks: 0,
+            noClicks: 0
+        };
+        this.basicMarkup = "\n<prompt (yes)=\"addYes()\" (no)=\"addNo()\">\n    <prompt-header>\n        <h4>Basic Prompt</h4>\n    </prompt-header>\n    <prompt-body>\n        This is the body of the prompt.\n    </prompt-body>\n    <prompt-confirm (click)=\"addYes()\">Yes</prompt-confirm>\n    <prompt-cancel (click)=\"addNo()\">No</prompt-cancel>\n</prompt>\n    ";
+        this.modalMarkup = "\n<prompt-modal (confirm)=\"addYes()\" (cancel)=\"addNo()\" #modal>\n    <prompt-header>\n        <h4>Modal Prompt</h4>\n    </prompt-header>\n    <prompt-body>\n        This is the body of the prompt.\n    </prompt-body>\n    <prompt-confirm>Yes</prompt-confirm>\n    <prompt-cancel>No</prompt-cancel>\n</prompt-modal>\n    ";
+    }
+    PromptDemoComponent.prototype.addYes = function () {
+        this.model.yesClicks++;
+    };
+    PromptDemoComponent.prototype.addNo = function () {
+        this.model.noClicks++;
+    };
+    PromptDemoComponent = __decorate([
+        Object(__WEBPACK_IMPORTED_MODULE_0__angular_core__["n" /* Component */])({
+            selector: 'prompt-demo',
+            template: "\n        <div class=\"container\">\n            <h3>Prompts</h3>\n            <div class=\"row\">\n                <div class=\"col-3\">\n                    <div class=\"nav flex-column nav-pills\" aria-orientation=\"vertical\" role=\"tablist\">\n                        <a class=\"nav-link active\" data-toggle=\"pill\" href=\"#basic\" role=\"tab\">\n                            Basic Prompt\n                        </a>\n                        <a class=\"nav-link\" data-toggle=\"pill\" href=\"#modal\" role=\"tab\">\n                            Modal Prompt\n                        </a>\n                    </div>\n                </div>\n                <div class=\"col-9\">\n                    <div class=\"tab-content\">\n                        <div class=\"tab-pane active\" id=\"basic\" role=\"tabpanel\">\n                            <div class=\"col-12\">\n                                <prompt (yes)=\"addYes()\" (no)=\"addNo()\">\n                                    <prompt-header>\n                                        <h4>Basic Prompt</h4>\n                                    </prompt-header>\n                                    <prompt-body>\n                                        This is the body of the prompt.\n                                    </prompt-body>\n                                    <prompt-confirm (click)=\"addYes()\">Yes</prompt-confirm>\n                                    <prompt-cancel (click)=\"addNo()\">No</prompt-cancel>\n                                </prompt>\n\n                                <pre class=\"card card-body bg-light\">{{basicMarkup}}</pre>\n                                <pre class=\"card card-body bg-light\">{{ model | json }}</pre>\n                            </div>\n                        </div>\n                        <div class=\"tab-pane\" id=\"modal\" role=\"tabpanel\">\n                            <div class=\"col-12\">\n                                <prompt-modal (confirm)=\"addYes()\" (cancel)=\"addNo()\" #modal>\n                                    <prompt-header>\n                                        <h4>Modal Prompt</h4>\n                                    </prompt-header>\n                                    <prompt-body>\n                                        This is the body of the prompt.\n                                    </prompt-body>\n                                    <prompt-confirm>Yes</prompt-confirm>\n                                    <prompt-cancel>No</prompt-cancel>\n                                </prompt-modal>\n                                <div class=\"form-group\">\n                                    <button class=\"btn btn-primary\" (click)=\"modal.show()\">Open Modal</button>\n                                </div>\n                                <pre class=\"card card-body bg-light\">{{modalMarkup}}</pre>\n                                <pre class=\"card card-body bg-light\">{{ model | json }}</pre>\n                            </div>\n                        </div>\n                    </div>\n                </div>\n            </div>\n        </div>\n    "
+        })
+    ], PromptDemoComponent);
+    return PromptDemoComponent;
+}());
+
+
+
+/***/ }),
+
+/***/ "../../../../../src/app/Pages/IndexPageComponent.ts":
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+/* harmony export (binding) */ __webpack_require__.d(__webpack_exports__, "a", function() { return IndexPageComponent; });
+/* harmony import */ var __WEBPACK_IMPORTED_MODULE_0__angular_core__ = __webpack_require__("../../../core/esm5/core.js");
+var __decorate = (this && this.__decorate) || function (decorators, target, key, desc) {
+    var c = arguments.length, r = c < 3 ? target : desc === null ? desc = Object.getOwnPropertyDescriptor(target, key) : desc, d;
+    if (typeof Reflect === "object" && typeof Reflect.decorate === "function") r = Reflect.decorate(decorators, target, key, desc);
+    else for (var i = decorators.length - 1; i >= 0; i--) if (d = decorators[i]) r = (c < 3 ? d(r) : c > 3 ? d(target, key, r) : d(target, key)) || r;
+    return c > 3 && r && Object.defineProperty(target, key, r), r;
+};
+var __metadata = (this && this.__metadata) || function (k, v) {
+    if (typeof Reflect === "object" && typeof Reflect.metadata === "function") return Reflect.metadata(k, v);
+};
+
+var IndexPageComponent = /** @class */ (function () {
+    function IndexPageComponent() {
+    }
+    IndexPageComponent = __decorate([
+        Object(__WEBPACK_IMPORTED_MODULE_0__angular_core__["n" /* Component */])({
+            selector: 'index-page',
+            template: "\n        <div class=\"container\">\n            <h3>Index</h3>\n        </div>\n    "
+        }),
+        __metadata("design:paramtypes", [])
+    ], IndexPageComponent);
+    return IndexPageComponent;
+}());
+
+
+
+/***/ }),
+
+/***/ "../../../../../src/app/routes.ts":
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+/* harmony export (binding) */ __webpack_require__.d(__webpack_exports__, "a", function() { return routes; });
+/* harmony import */ var __WEBPACK_IMPORTED_MODULE_0__ng_ui_libraries_forms__ = __webpack_require__("../../../../@ng-ui-libraries/forms/@ng-ui-libraries/forms.es5.js");
+/* harmony import */ var __WEBPACK_IMPORTED_MODULE_1__Component_PromptDemoComponent__ = __webpack_require__("../../../../../src/app/Component/PromptDemoComponent.ts");
+/* harmony import */ var __WEBPACK_IMPORTED_MODULE_2__Pages_IndexPageComponent__ = __webpack_require__("../../../../../src/app/Pages/IndexPageComponent.ts");
+
+
+
+var routes = [
+    {
+        path: 'prompt',
+        component: __WEBPACK_IMPORTED_MODULE_1__Component_PromptDemoComponent__["a" /* PromptDemoComponent */]
+    },
+    {
+        path: 'forms',
+        component: __WEBPACK_IMPORTED_MODULE_0__ng_ui_libraries_forms__["a" /* FormsDemoComponent */]
+    },
+    {
+        path: '',
+        component: __WEBPACK_IMPORTED_MODULE_2__Pages_IndexPageComponent__["a" /* IndexPageComponent */]
+    }
+];
 
 
 /***/ }),
